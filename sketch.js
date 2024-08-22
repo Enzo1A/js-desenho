939526870db1da29273f7b1f37259388c1f3a458
Enzo1A <00001115095985SP@al.educacao.sp.gov.br>
2024-08-22 15:19:28 -0300

    atualizando arquivos

--- sketch.js
+++ sketch.js
@@ -1,27 +1,31 @@
 let cor;
-let posicaoHorizontal; // x
-let posicaoVertical; // y
-
-  function setup() {
-  createCanvas(400,400);
-  background(100, 0, 0);
-  cor= color(random(0, 255), random(0, 255), random(0, 255));          posicaoHorizontal=200;
-  posicaovertical=200;
-  }
- 
+let circuloX; //horizontal
+let circuloY; //vertical
 
 
 function setup() {
+   createCanvas(700, 500);
+   background(color("white")); 
+cor=color(random(0,255), random(0,255), random(0,255))
   
-  fill(cor);
-  circle(posicaoHorizontal, posicaoVertical, 50);
-  
-  if (mouseX<posicaoHorizontal {
-      posicaoHorizontal= posicaoHorizontal - 1;
-      }
+  circuloX= [0,0,0];
+  circuloY= [random(height), random(height), random(height)] 
+}
 
-  if (mouseX>posicaoHorizontal){
-    posicaoHorizontal= posicaoHorizontal + 1;
+function draw() {
+   fill(cor);
+   for(let contador in circuloX){
+     circle(circuloX[contador],circuloY[contador], 50);
+     circuloX[contador]+=random(0,3);
+     circuloY[contador]+=random(-3,3);
+     
+     if(circuloX[contador]>=width){
+       circuloX[contador]=0;
+       circuloY[contador]=random(height);
+    }
   }
-  
-  if(mouseX)
+ 
+ if (mouseIsPressed) {
+     cor = color(random(0,255), random(0,255), random(0,255));
+   }
+ }
